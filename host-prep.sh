01c6df994b4db231b58e1590fc3817f097ff4221
pete <pete@petervogt.net>
2025-08-17 17:17:27 +0200

    Add proxy cleanup instructions to host-prep.sh

--- host-prep.sh
+++ host-prep.sh
@@ -5,6 +5,10 @@
 # sudo sed -i '/cdrom/d' /etc/apt/sources.list
 # printf "deb http://deb.debian.org/debian trixie main contrib non-free-firmware\ndeb http://deb.debian.org/debian trixie-updates main contrib non-free-firmware\ndeb http://security.debian.org/debian-security trixie-security main contrib non-free-firmware\ndeb http://deb.debian.org/debian trixie-backports main contrib non-free-firmware\n" | sudo tee /etc/apt/sources.list > /dev/null
 # sudo apt install git
+# 
+# Once done with installs, remove the proxy:
+# sudo rm /etc/apt/apt.conf.d/80proxy
+# sudo sed -i '/http_proxy/d' /etc/environment
 
 HOST=$(awk -F= '/^ID=/ {print $2}' /etc/os-release | tr -d '"')
 VERSION_CODENAME=$(awk -F= '/^VERSION_CODENAME=/ {print $2}' /etc/os-release | tr -d '"')
