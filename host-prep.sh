b1db934c11194d87ed7f5ee56acdc06a00f1c32f
pete <pete@petervogt.net>
2025-08-17 16:02:38 +0200

    add debian trixie fresh install steps

--- host-prep.sh
+++ host-prep.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 # Host preparation script for Ansible playbook execution
+#
+# On a fresh trixie install you may need
+# printf "deb http://deb.debian.org/debian trixie main contrib non-free-firmware\ndeb http://deb.debian.org/debian trixie-updates main contrib non-free-firmware\ndeb http://security.debian.org/debian-security trixie-security main contrib non-free-firmware\ndeb http://deb.debian.org/debian trixie-backports main contrib non-free-firmware\n" | sudo tee /etc/apt/sources.list > /dev/null
+# sudo apt install git
 
 HOST=$(awk -F= '/^ID=/ {print $2}' /etc/os-release | tr -d '"')
+VERSION_CODENAME=$(awk -F= '/^VERSION_CODENAME=/ {print $2}' /etc/os-release | tr -d '"')
 PROXY="http://192.168.68.127:3128"
 PLAYBOOK="https://github.com/apnea/cepat.git"
 USEPROXY=true
 
-echo "Detected host as $HOST"
+echo "Detected host as $HOST and version as $VERSION_CODENAME"
 echo "Proxy set to $PROXY"
 
 echo "\nTesting proxy..."
@@ -22,16 +27,16 @@ case "$HOST" in
         sudo apt install -y ansible ansible-lint
         sudo apt install -y python3-pip
         if [ "$USEPROXY" = true ]; then
-            echo 'Acquire::http::Proxy $PROXY;' |sudo tee /etc/apt/apt.conf.d/80proxy
-            echo 'http_proxy "http://192.168.68.127:3128' | sudo tee /etc/environment
+            echo "Acquire::http::Proxy $PROXY;" |sudo tee /etc/apt/apt.conf.d/80proxy
+            echo "http_proxy $PROXY;" | sudo tee /etc/environment
         fi
         ;;
     debian)
         sudo apt update
         sudo apt install -y ansible ansible-lint
         if [ "$USEPROXY" = true ]; then
-            echo 'Acquire::http::Proxy $PROXY;' |sudo tee /etc/apt/apt.conf.d/80proxy
-            echo 'http_proxy "http://192.168.68.127:3128' | sudo tee /etc/environment
+            echo "Acquire::http::Proxy $PROXY;" |sudo tee /etc/apt/apt.conf.d/80proxy
+            echo "http_proxy $PROXY;" | sudo tee /etc/environment
         fi
         ;;
     arch)
